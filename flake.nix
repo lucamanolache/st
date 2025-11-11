59ddabce002f1134b8829833172152dfc64fda5d
Luca Manolache <luca1230@gmail.com>
2025-11-10 19:21:13 -0800

    Flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "My fork of the 'st' terminal";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      pkgsFor = system: import nixpkgs { inherit system; };
+
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          st = pkgs.stdenv.mkDerivation rec {
+            pname = "st";
+            version = "git-flake";
+
+            src = self;
+
+            buildInputs = with pkgs; [
+              libX11
+              libXft
+              fontconfig
+              harfbuzz
+            ];
+
+            installPhase = ''
+              make install PREFIX=$out
+            '';
+          };
+          
+          default = self.packages.${system}.st;
+        });
+      
+      defaultPackage = forAllSystems (system: self.packages.${system}.default);
+    };
+}
